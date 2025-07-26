efc328808fd9f6dbd2da0a4ad45382d279ad4d22
Jonatan-ESG <jona.sandovalguerra@gmail.com>
2025-07-26 15:02:13 -0600

    :construction_worker: Creación de cuenta bancaria

---
+++ Clases/CuentaBancaria.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilaresPOO.Clases
+{
+	public class CuentaBancaria
+	{
+		private decimal saldo;
+		private string nombreUsuario;
+		private string password;
+		private List<string> movimientos;
+
+		public CuentaBancaria (string usuario, string password, decimal saldoInicial = 0)
+		{
+			this.nombreUsuario = usuario;
+			this.password = password;
+			this.saldo = saldoInicial;
+		}
+
+		public void Depositar(decimal montoDeposito) { 
+
+			if(montoDeposito > 0)
+			{
+				saldo += montoDeposito;
+			}
+
+		}
+
+		public decimal ObtenerSaldo(string usuario, string password) {
+			
+			if (usuario != this.nombreUsuario || password != this.password) {
+				return 0;
+			}
+
+			return saldo;
+		}
+
+
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
-﻿internal class Program
+﻿using PilaresPOO.Clases;
+
+internal class Program
 {
 	private static void Main(string[] args)
 	{
-		Console.WriteLine("Hello, World!");
+		CuentaBancaria cuentaNueva = new CuentaBancaria("jsandoval", "12345678", 300);
+
+		cuentaNueva.Depositar(100);
+
+		Console.WriteLine(cuentaNueva.ObtenerSaldo("jsandval", "12345678"));
 	}
 }
